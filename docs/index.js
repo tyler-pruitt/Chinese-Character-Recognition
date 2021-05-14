07870aa562da57ffc25d897ea8a6d58a99b1967e
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-05-14 04:08:47 -0700

    Add files via upload

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 //Your JavaScript code will go here!
 window.onload = function() {
     
-    //loadScript('https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js');
+    loadScript('https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js');
 
     var canvas = document.querySelector("canvas");
 
