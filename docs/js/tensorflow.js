4c0d122294ee7b86a0d2785fad7d977de7a26a3c
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-07-02 14:32:47 -0700

    Update bar plot

--- docs/js/tensorflow.js
+++ docs/js/tensorflow.js
@@ -122,7 +122,43 @@ var predict = function(input) {
       console.log("Model Input:");
       console.log(tf.tensor(input).reshape([1, 64, 64]).data());
 
-      barplot(scores);
+      // Bar Plot
+      anychart.onDocumentReady(function(scores) {
+
+        // Set the data
+        var data = {
+            header: ["Character", "Score"],
+            rows: [
+            ["零", scores[0]],
+            ["一", scores[1]],
+            ["二", scores[2]],
+            ["三", scores[3]],
+            ["四", scores[4]],
+            ["五", scores[5]],
+            ["六", scores[6]],
+            ["七", scores[7]],
+            ["八", scores[8]],
+            ["九", scores[9]],
+            ["十", scores[10]],
+            ["百", scores[11]],
+            ["千", scores[12]],
+            ["万", scores[13]],
+            ["亿", scores[14]],
+        ]};
+        
+        // Create the chart
+        var chart = anychart.column();
+        
+        // Add data
+        chart.data(data);
+        
+        // Set the chart title
+        chart.title("");
+        
+        // Draw
+        chart.container("container");
+        chart.draw();
+    });
 
       // Process the data
       scores = scores[0];
