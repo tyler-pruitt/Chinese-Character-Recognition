6145b5c2361c9ba06862ec50d20aca0d363b68e6
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-07-04 04:50:36 -0700

    Enable mobile use

--- docs/js/tensorflow.js
+++ docs/js/tensorflow.js
@@ -120,6 +120,23 @@ canvas.addEventListener('touchmove', function (e) {
   }));
 }, false);
 
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+
 // Prediction function
 var predict = function(input) {
   if (window.model) {
