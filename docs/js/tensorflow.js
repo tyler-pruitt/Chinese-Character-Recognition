cddaa538289b045a751f56d7ede8a04528152d3d
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-07-04 04:37:55 -0700

    Improve documentation and naming

--- docs/js/tensorflow.js
+++ docs/js/tensorflow.js
@@ -1,22 +1,24 @@
+// Determine if the user is using a mobile device or not
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
+// Set up mobile settings
 if (isMobile) {
   $('#paint').css({'width': '60%'});
-  $('#number').css({'width': '30%', 'font-size': '240px'});
-  $('#predict_button').css({'font-size': '50px'});
-  $('#clear').css({'font-size': '50px'});
+  $('#bigCharacter').css({'width': '30%', 'font-size': '240px'});
+  $('#predictButton').css({'font-size': '50px'});
+  $('#clearButton').css({'font-size': '50px'});
 } else {
   $('#paint').css({'width': '300px'});
-  $('#number').css({'width': '150px', 'font-size': '120px'});
-  $('#predict_button').css({'font-size': '20px'});
-  $('#clear').css({'font-size': '20px'});
+  $('#bigCharacter').css({'width': '150px', 'font-size': '120px'});
+  $('#predictButton').css({'font-size': '20px'});
+  $('#clearButton').css({'font-size': '20px'});
 }
 
 var cw = $('#paint').width();
 $('#paint').css({'height': cw + 'px'});
 
-cw = $('#number').width();
-$('#number').css({'height': cw + 'px'});
+cw = $('#bigCharacter').width();
+$('#bigCharacter').css({'height': cw + 'px'});
 
 // From https://www.html5canvastutorials.com/labs/html5-canvas-paint-application/
 var canvas = document.getElementById('myCanvas');
@@ -28,15 +30,18 @@ canvas.height = parseInt(compuetedStyle.getPropertyValue('height'));
 
 var mouse = {x: 0, y: 0};
 
+// Write on the canvas when the mouse is moving
 canvas.addEventListener('mousemove', function(e) {
   mouse.x = e.pageX - this.offsetLeft;
   mouse.y = e.pageY - this.offsetTop;
 }, false);
 
+// Adjust line width and shape
 context.lineWidth = isMobile ? 40 : 10;
 context.lineJoin = 'round';
 context.lineCap = 'round';
 
+// Set stroke color to white
 // Blue: '#0000FF', Black: '#000000', White: '#ffffff'
 context.strokeStyle = '#ffffff';
 
@@ -44,19 +49,24 @@ context.strokeStyle = '#ffffff';
 context.fillStyle='black';
 context.fillRect(0,0,canvas.width,canvas.height);
 
+// Start writing on the canvas when mouse is pressed down
 canvas.addEventListener('mousedown', function(e) {
   context.moveTo(mouse.x, mouse.y);
   context.beginPath();
   canvas.addEventListener('mousemove', onPaint, false);
 }, false);
 
+// Stop writing on the canvas when the mouse is lifted up
 canvas.addEventListener('mouseup', function() {
   canvas.removeEventListener('mousemove', onPaint, false);
 }, false);
 
-$('#predict_button').click(function(){
-  $('#number').html('<img id="spinner" src="spinner.gif"/>');
+// Predict button
+$('#predictButton').click(function(){
+  // Insert loading spinner into number slot
+  $('#bigCharacter').html('<img id="spinner" src="spinner.gif"/>');
   
+  // Retrieve image data
   var img = new Image();
   img.onload = function() {
     context.drawImage(img, 0, 0, 64, 64);
@@ -67,9 +77,11 @@ $('#predict_button').click(function(){
       input.push(data[i + 2] / 255);
     }
 
-    console.log("Image Data:");
+    // Model Input Data
+    console.log("Model Input:");
     console.log(input);
 
+    // Predict character given image data
     predict(input);
   };
   img.src = canvas.toDataURL('image/png');
@@ -80,6 +92,7 @@ var onPaint = function() {
   context.stroke();
 };
 
+// Load the model
 tf.loadLayersModel('model/model.json').then(function(model) {
   window.model = model;
   console.log("Model Loaded Successfully.");
@@ -107,13 +120,11 @@ canvas.addEventListener('touchmove', function (e) {
   }));
 }, false);
 
+// Prediction function
 var predict = function(input) {
   if (window.model) {
+    // If the model is loaded, make a prediction with reshaped input
     window.model.predict([tf.tensor(input).reshape([1, 64, 64])]).array().then(function(scores){
-      // Model Input Data
-      console.log("Model Input:");
-      console.log(tf.tensor(input).reshape([1, 64, 64]).data());
-
       // Process the data
       scores = scores[0];
 
@@ -121,26 +132,35 @@ var predict = function(input) {
       console.log("Model Output:");
       console.log(scores);
 
+      // Determine the highest score's index
       var predictedIndex = scores.indexOf(Math.max(...scores));
 
       var characters = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '百', '千', '万', '亿'];
 
+      // Determine the predicted character and output to website and console
       var predictedCharacter = characters[predictedIndex];
       $('#character').html(predictedCharacter);
       console.log("Predicted Character: " + predictedCharacter);
 
       var probability = scores[predictedIndex] * 100;
+
       var probabilityDisplay = probability.toString() + "%";
-      $('#probability').html(probabilityDisplay);
       console.log("Probability: " + probabilityDisplay);
 
-      $('#number').html(predictedCharacter);
+      // Round probability to three decimals
+      const roundedProbability = Math.round(probability * 1000) / 1000;
+      var roundedProbabilityDisplay = roundedProbability.toString() + "%";
+      $('#probability').html(roundedProbabilityDisplay);
 
-      // Update bar plot with data (first remove previous data and then add new data)
+      $('#bigCharacter').html(predictedCharacter);
+
+      // Update bar plot with data
+      // First remove previous data
       for (var i=0;i<15;i+=1) {
         removeData(barchart);
       }
 
+      // Add new data
       for (var i=0;i<15;i+=1) {
         addData(barchart, characters[i], scores[i]);
       }
@@ -151,7 +171,8 @@ var predict = function(input) {
   }
 }
 
-$('#clear').click(function(){
+// Clear button
+$('#clearButton').click(function(){
   context.clearRect(0, 0, canvas.width, canvas.height);
   
   // If canvas is black, replace black color after clearing the canvas
@@ -160,13 +181,14 @@ $('#clear').click(function(){
 
   $('#character').html('');
   $('#probability').html('');
-  $('#number').html('');
+  $('#bigCharacter').html('');
 
-  // Remove existing data and insert zeros
+  // Remove existing data in bar chart
   for (var i=0;i<15;i+=1) {
     removeData(barchart);
   }
   
+  // Insert 0's for data in bar chart (i.e. reset the bar chart)
   var characters = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '百', '千', '万', '亿'];
 
   for (var i=0;i<15;i+=1) {
